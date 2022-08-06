4312459e81d6a09bf102c26b233f210185fd69a9
CaroBima <82395515+CaroBima@users.noreply.github.com>
2022-08-06 13:17:24 -0300

    Agregando funcionalidad al boton limpiar busqueda, borra el texto escrito en el imput

--- src/app/components/cursos/cursos.component.ts
+++ src/app/components/cursos/cursos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 import { ICursoInterface } from 'src/app/models/CursoInterface';
 import { CursosService } from 'src/app/services/cursos.service';
@@ -10,13 +10,21 @@ import { CursosService } from 'src/app/services/cursos.service';
 })
 export class CursosComponent implements OnInit {
   cursos: ICursoInterface[] = [];
+  buscador: any;
   constructor(private _cursosService: CursosService) {}
 
   ngOnInit(): void {
     this.cursos = this._cursosService.getCursos();
   }
 
+  //permite traer los cursos de acuerdo al string escrito en el buscador
   public buscarTecnologia(tecnologia: string) {
     this.cursos = this._cursosService.buscadorCurso(tecnologia);
+  }
+
+  //metodo del boton busqueda para limpiar la busqueda y traer todos los cursos nuevamente
+  limpiarBusqueda() {
+    this.cursos = this._cursosService.getCursos();
+    this.buscador = '';
   }
 }
