cf994b5b3487e3f9a9031d1f51c05dbe5d291742
CaroBima <82395515+CaroBima@users.noreply.github.com>
2022-08-11 09:24:36 -0300

    agregando información en la pagina principal

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,13 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public show: boolean[] = [];
+  //public showOrNot: string;
+  constructor() {
+    //this.showOrNot = 'Mostrar';
+    this.show = [false, false];
   }
 
+  ngOnInit(): void {}
+
+  showData(cualMostrar: string) {
+    /*if (this.show) {
+      this.showOrNot = valor;
+    } else {
+      this.showOrNot = `Ocultar ${valor}`;
+    }*/
+    switch (cualMostrar) {
+      case 'dedonde':
+        this.show[0] = !this.show[0];
+        break;
+      case 'formacion':
+        this.show[1] = !this.show[1];
+        break;
+    }
+  }
 }
