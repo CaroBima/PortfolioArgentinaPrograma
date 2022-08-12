19752a125810b9df3643e5ca36bd0e952d7a620a
CaroBima <82395515+CaroBima@users.noreply.github.com>
2022-08-11 21:36:09 -0300

    Terminando de agregar info al home y dandole estilos

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,7 +10,7 @@ export class HomeComponent implements OnInit {
   //public showOrNot: string;
   constructor() {
     //this.showOrNot = 'Mostrar';
-    this.show = [false, false];
+    this.show = [false, false, false];
   }
 
   ngOnInit(): void {}
@@ -21,12 +21,22 @@ export class HomeComponent implements OnInit {
     } else {
       this.showOrNot = `Ocultar ${valor}`;
     }*/
+    //por el momento al ser pocas opciones lo dejo con un switch, seteando a false los otros botones
     switch (cualMostrar) {
       case 'dedonde':
         this.show[0] = !this.show[0];
+        this.show[1] = false;
+        this.show[2] = false;
         break;
       case 'formacion':
+        this.show[0] = false;
         this.show[1] = !this.show[1];
+        this.show[2] = false;
+        break;
+      case 'trabajo':
+        this.show[0] = false;
+        this.show[1] = false;
+        this.show[2] = !this.show[2];
         break;
     }
   }
