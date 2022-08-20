fe0f9087dbb9e3082c21449a57d3fadfc8313c3c
CaroBima <82395515+CaroBima@users.noreply.github.com>
2022-08-20 05:00:48 -0300

    Finalizado endpoint que permite guardar una nueva tecnologia, validando que no exista en la bbdd

--- backend/src/main/java/portfolio/portfolioBack/controller/TecnologiaController.java
+++ backend/src/main/java/portfolio/portfolioBack/controller/TecnologiaController.java
@@ -1,6 +1,7 @@
 
 package portfolio.portfolioBack.controller;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,21 @@ public class TecnologiaController {
     
     @PostMapping("/nuevaTecnologia")
     public void guardarTecnologia(@RequestBody Tecnologia tecnologia){
+        //traigo la lista de tecnologías y reviso si la tecnología ya se encuentra guardada
+        List<Tecnologia> listaTecnologias= tecnologiaService.traerTecnologias();
+        boolean yaEsta = false;
+        //recorro el array para ver si la tecnologia ya se encuentra guardada
+        for( Tecnologia tecno : listaTecnologias){
+            if(tecno.getNombreTecnologia().equals(tecnologia.getNombreTecnologia())){
+               yaEsta = true; 
+            }
+        }
+        //si no esta en el listado la guardo
+        if(!yaEsta){
         tecnologiaService.guardarTecnologia(tecnologia);
+        }else{//console log de prueba
+            System.out.println("La tecnologia ya esta");
+        }
+        
     }
 }
