4b24544bb09c8366009917f0e0289288696f970c
CaroBima <82395515+CaroBima@users.noreply.github.com>
2022-10-21 11:53:30 -0300

    Comenzando con la seguridad

--- backend/src/main/java/portfolio/portfolioBack/controller/UsuarioController.java
+++ backend/src/main/java/portfolio/portfolioBack/controller/UsuarioController.java
@@ -1,13 +1,24 @@
 
 package portfolio.portfolioBack.controller;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import portfolio.portfolioBack.model.Usuario;
 import portfolio.portfolioBack.service.IUsuarioService;
+//
+//import io.jsonwebtoken.Jwts;
+//import io.jsonwebtoken.SignatureAlgorithm;
+
 
 @CrossOrigin
 @RestController
@@ -16,7 +27,42 @@ public class UsuarioController {
     IUsuarioService usuarioService;
     
     @PostMapping("/login")
-    private boolean loginUsuario(@RequestBody Usuario usuario){
-       return usuarioService.logueoUsuario(usuario);
+    private String loginUsuario(@RequestBody Usuario usuario){
+        Usuario usu = new Usuario();
+        String token;
+       //valido si el usuario y pass estan ok, si es así genero el token
+       if(usuarioService.logueoUsuario(usuario)){
+            usuario.setNombreUsuario(usuario.getNombreUsuario());
+            token = this.getJWTToken(usuario.getNombreUsuario());
+        }
+       else{
+           token = null;
+       }
+    
+       return token;
     }
+    
+ 
+    
+	private String getJWTToken(String username) {
+	   	String secretKey = "mySecretKey";
+		List<GrantedAuthority> grantedAuthorities = AuthorityUtils
+				.commaSeparatedStringToAuthorityList("ROLE_USER");
+		
+		String token = Jwts
+				.builder()
+				.setId("softtekJWT")
+				.setSubject(username)
+				.claim("authorities",
+						grantedAuthorities.stream()
+								.map(GrantedAuthority::getAuthority)
+								.collect(Collectors.toList()))
+				.setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis() + 600000))
+				.signWith(SignatureAlgorithm.HS512,
+						secretKey.getBytes()).compact();
+            
+		return "Bearer "; // + token;
+	}
+    
 }
