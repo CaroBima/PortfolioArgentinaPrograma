5ea7768edc1d32102843e05d38cb269d6eae939e
CaroBima <82395515+CaroBima@users.noreply.github.com>
2022-12-16 13:28:09 -0300

    Agregando metodos para cargar la imagen al componente de nuevo proyecto

--- frontend/src/app/components/nuevo-proyecto/nuevo-proyecto.component.ts
+++ frontend/src/app/components/nuevo-proyecto/nuevo-proyecto.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { SubirArchivosService } from 'src/app/services/subirArchivos.service';
 
 @Component({
   selector: 'app-nuevo-proyecto',
   templateUrl: './nuevo-proyecto.component.html',
-  styleUrls: ['./nuevo-proyecto.component.css']
+  styleUrls: ['./nuevo-proyecto.component.css'],
 })
 export class NuevoProyectoComponent implements OnInit {
+  public previsualizacion: string = '';
+  public file!: File; // Variable to store file
 
-  constructor() { }
+  constructor(
+    private sanitizer: DomSanitizer,
+    private subirArchivosService: SubirArchivosService
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  //------------------------------------------ Cargado de img ----------------------------------------------------
+  // Al seleccionar la imagen a cargar
+  onChange(event: any) {
+    this.file = event.target.files[0];
+    this.extraerBase64(this.file).then((imagen: any) => {
+      this.previsualizacion = imagen.base;
+    });
   }
 
+  onUpload() {
+    this.subirArchivosService.subirImagen(this.file);
+  }
+
+  extraerBase64 = async ($event: any) =>
+    new Promise((resolve, reject) => {
+      try {
+        const unsafeImg = window.URL.createObjectURL($event);
+        const image = this.sanitizer.bypassSecurityTrustUrl(unsafeImg);
+        const reader = new FileReader();
+        reader.readAsDataURL($event);
+        reader.onload = () => {
+          resolve({
+            base: reader.result,
+          });
+        };
+        reader.onerror = (error) => {
+          resolve({
+            base: null,
+          });
+        };
+      } catch (e) {
+        //return null;
+      }
+    });
 }
